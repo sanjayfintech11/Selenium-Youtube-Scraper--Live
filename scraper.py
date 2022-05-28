63d47d362c78ed92adc03200325328f58c56695b
Sanjay Veeraragavulu <85775960+sanjayfintech11@users.noreply.github.com>
2022-05-28 10:16:34 +0000

    LEts Commit and push to my Buddy

--- scraper.py
+++ scraper.py
@@ -1,1 +1,5 @@
-print("Hello World")
+import requests
+YOUTUBE_TRENDING_URL='https://www.youtube.com/feed/trending'
+response = requests.get(YOUTUBE_TRENDING_URL)
+print('Status Code', response.status_code)
+print('Output',response.text)
